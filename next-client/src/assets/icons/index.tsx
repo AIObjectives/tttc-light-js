5d65dba550bbc3445aa28cd54e09c59b999fcf6a
Brandon Perry <brandonperryofficial@gmail.com>
2024-05-17 11:52:32 -0700

    added lucide icons

---
+++ next-client/src/assets/icons/index.tsx
@@ -0,0 +1,16 @@
+import {
+  Link,
+  BookText,
+  CircleUser,
+  Text,
+  Calendar,
+  UnlinkIcon,
+} from "lucide-react";
+
+const Icons = () => <></>;
+
+const Copy = Link;
+
+Icons.Copy = Copy;
+
+export default Icons;
