6614e7c438cd4f296ad90bcbfb0bd7af2b1d8cc1
Brandon Perry <brandonperryofficial@gmail.com>
2024-05-17 14:14:35 -0700

    implemented Inter typography

--- next-client/.storybook/ThemeProvider.tsx
+++ next-client/.storybook/ThemeProvider.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
+import { nextTypography } from "../src/lib/font";
 
 const ThemeProvider = ({
   children,
   theme,
 }: React.PropsWithChildren<{ theme: "light" | "dark" }>) => {
+  document.body.className = document.body.className.concat(
+    `, ${nextTypography}`,
+  );
   useEffect(() => {
     if (theme === "dark") {
       document.body.classList.add("dark");
