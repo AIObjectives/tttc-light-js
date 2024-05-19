d8ae1831ae7593a15eef3a88950bcebf17c717ef
Brandon Perry <brandonperryofficial@gmail.com>
2024-05-19 13:44:07 -0700

    made changes to topic and claim

--- next-client/src/components/topic/Topic.stories.tsx
+++ next-client/src/components/topic/Topic.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Topic from "./Topic";
 import { taxonomyObject } from "stories/data/dummyData";
+import { Card, CardContent } from "../elements";
 
 const meta = {
   title: "Topic",
@@ -9,11 +10,20 @@ const meta = {
     // layout: "centered",
   },
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <Card>
+        <CardContent>
+          <Story />
+        </CardContent>
+      </Card>
+    ),
+  ],
 } satisfies Meta<typeof Topic>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: taxonomyObject[0].subtopics[0],
+  args: { subtopic: taxonomyObject[0].subtopics[0] },
 };
