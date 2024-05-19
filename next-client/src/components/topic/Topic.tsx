5722b64f866dc1fc9e937133cffbebfab930925f
Brandon Perry <brandonperryofficial@gmail.com>
2024-05-19 13:44:07 -0700

    made changes to topic and claim

--- next-client/src/components/topic/Topic.tsx
+++ next-client/src/components/topic/Topic.tsx
@@ -1,39 +1,29 @@
 import React from "react";
 import * as schema from "tttc-common/schema";
 import CopyLinkButton from "../copyLinkButton/CopyLinkButton";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../elements";
+import { CardDescription } from "../elements";
 import PointGraphic from "../pointGraphic/PointGraphic";
 import Claim from "../claim/Claim";
 
-function Topic({
-  subtopicName,
-  subtopicShortDescription,
-  claims,
-}: schema.Subtopic) {
+function Topic({ subtopic }: { subtopic: schema.Subtopic }) {
+  const { subtopicName, claims, claimsCount, subtopicShortDescription } =
+    subtopic;
   return (
-    <Card>
-      <CardContent className="flex flex-col gap-y-4">
-        <TopicHeader title={subtopicName} numClaims={10} numPeople={10} />
-        <PointGraphic num={claims!.length} />
-        <p>{subtopicShortDescription}</p>
-        <div className="flex flex-col gap-y-6">
-          {claims!.map((claim, i) => (
-            // ! Something wrong with quotes here
-            <Claim
-              claimNum={i}
-              title={claim.topicName}
-              quotes={[claim.quote]}
-            />
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+    <div className="flex flex-col gap-y-4">
+      <TopicHeader
+        title={subtopicName}
+        numClaims={claimsCount!}
+        numPeople={claims!.length}
+      />
+      <PointGraphic num={claims!.length} />
+      <p>{subtopicShortDescription}</p>
+      <div className="flex flex-col gap-y-6">
+        {claims!.map((claim, i) => (
+          // ! Something wrong with quotes here
+          <Claim claimNum={i} title={claim.topicName} quotes={[claim.quote]} />
+        ))}
+      </div>
+    </div>
   );
 }
 
@@ -48,7 +38,7 @@ function TopicHeader({
 }) {
   return (
     <div className="flex flex-row justify-between items-center">
-      <CardTitle className="text-xl">{title}</CardTitle>
+      <h3 className="text-xl font-semibold">{title}</h3>
       <div className="flex flex-row">
         <CardDescription className="self-center mr-4">
           {numClaims} claims by {numPeople} people
