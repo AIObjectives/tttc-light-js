36854a4dda6f146cc5c2891e77e6590b5e929fb8
Brandon Perry <brandonperryofficial@gmail.com>
2024-05-20 12:47:59 -0700

    split theme stories into multiple components

--- next-client/src/components/theme/Theme.stories.tsx
+++ next-client/src/components/theme/Theme.stories.tsx
@@ -1,18 +1,50 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Theme from "./Theme";
+import Theme, { ThemeGraphic, ThemeHeader, TopicList } from "./Theme";
 import { taxonomyObject } from "stories/data/dummyData";
+import { Card, CardContent } from "../elements";
+import React from "react";
+
 const meta = {
   title: "Theme",
   component: Theme,
-  parameters: {
-    // layout: "centered",
-  },
+  parameters: {},
   tags: ["autodocs"],
 } satisfies Meta<typeof Theme>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+const baseProps = taxonomyObject[0];
+
+export const Main: Story = {
   args: taxonomyObject[0],
 };
+
+const CardWrap = ({ children }: React.PropsWithChildren) => (
+  <Card>
+    <CardContent>{children}</CardContent>
+  </Card>
+);
+
+export const Header = () => (
+  <CardWrap>
+    <ThemeHeader title={baseProps.topicName} />
+  </CardWrap>
+);
+
+export const Graphic = () => (
+  <CardWrap>
+    <ThemeGraphic
+      numClaims={baseProps.claimsCount!}
+      numPeople={baseProps.subtopics.length}
+    />
+  </CardWrap>
+);
+
+export const ListOfTopics = () => (
+  <CardWrap>
+    <TopicList
+      topics={baseProps.subtopics.map((subtopic) => subtopic.subtopicName)}
+    />
+  </CardWrap>
+);
