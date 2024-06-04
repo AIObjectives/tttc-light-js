6f98d5edadff4e0c70a292616d6b5d51a5cf839e
Brandon Perry <brandonperryofficial@gmail.com>
2024-06-04 13:32:40 -0700

    added new typography stylings for different tag elements

--- next-client/src/components/elements/card/Card.tsx
+++ next-client/src/components/elements/card/Card.tsx
@@ -33,14 +33,7 @@ const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      "text-2xl font-semibold leading-none tracking-tight",
-      className,
-    )}
-    {...props}
-  />
+  <h3 ref={ref} className={cn(className)} {...props} />
 ));
 CardTitle.displayName = "CardTitle";
 
