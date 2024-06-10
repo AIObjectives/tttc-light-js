04e019a4633f5fce3c1da93faeeabb046c620b50
Brandon Perry <brandonperryofficial@gmail.com>
2024-06-10 13:34:14 -0700

    changed date to string. Fixed error <div> within <p> when running dev version of app.

--- next-client/src/components/elements/textIcon/TextIcon.tsx
+++ next-client/src/components/elements/textIcon/TextIcon.tsx
@@ -1,4 +1,3 @@
-import { Row } from "@src/components/layout";
 import React from "react";
 
 function TextIcon({
@@ -6,11 +5,9 @@ function TextIcon({
   icon,
 }: React.PropsWithChildren<{ icon: React.ReactNode }>) {
   return (
-    <p className="p2 text-muted-foreground">
-      <Row gap={1}>
-        {icon}
-        {children}
-      </Row>
+    <p className="p2 text-muted-foreground flex gap-2">
+      {icon}
+      {children}
     </p>
   );
 }
