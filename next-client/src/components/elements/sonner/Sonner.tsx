8852cc904176cbcf39e383da7b913f6d6f1a0565
Brandon Perry <brandonperryofficial@gmail.com>
2024-07-01 12:58:56 -0700

    adjusted Sonner colors

--- next-client/src/components/elements/sonner/Sonner.tsx
+++ next-client/src/components/elements/sonner/Sonner.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
+import "../../../app/global.css";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
@@ -15,7 +16,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:bg-primary group-[.toaster]:text-primary group-[.toaster]:border-border group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
