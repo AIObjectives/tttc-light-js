b48279a46f4c769a77165832870651166d09cdca
Brandon Perry <brandonperryofficial@gmail.com>
2024-11-09 16:21:29 -0800

    added ts-ignore to prevent docker build error

--- next-client/src/lib/hooks/useOutsideClick.ts
+++ next-client/src/lib/hooks/useOutsideClick.ts
@@ -1,16 +1,11 @@
 "use client";
-import React, {
-  MouseEventHandler,
-  Ref,
-  RefObject,
-  SyntheticEvent,
-  useEffect,
-} from "react";
+import { RefObject, useEffect } from "react";
 
 function useOutsideClick(ref: RefObject<HTMLElement>, callBack: () => void) {
   useEffect(() => {
     // ! Code works, but TypeScript is throwing a type error here. Can't figure out the right event type. But it does work.
     const handleOutsideClick = (event: MouseEvent) =>
+      // @ts-ignore
       ref.current !== null && !ref.current.contains(event.target)
         ? callBack()
         : undefined;
