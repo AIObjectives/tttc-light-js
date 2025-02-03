3e859964cf5a050799f2310e33b45bf828e42f60
Brandon Perry <brandonperryofficial@gmail.com>
2025-02-03 13:13:24 -0800

    landing page except case studies

--- next-client/src/app/page.tsx
+++ next-client/src/app/page.tsx
@@ -1,23 +1,13 @@
+import Landing from "@src/components/landing/Landing";
+
 export function generateStaticParams() {
   return [{ slug: [""] }];
 }
 
 export default function HomePage() {
   return (
-    <>
-      <p className="intro">
-        Talk to the City (TttC) is a AI-powered summarization tool designed to
-        generate insightful reports based on public consultation data.
-      </p>
-
-      <div id="messageModal" className="modal hidden">
-        <div className="modal-content">
-          <span className="close-button">&times;</span>
-          <h2 id="modalTitle">Message Title</h2>
-          <p id="modalMessage">Your message goes here.</p>
-          <a id="modalLink"></a>
-        </div>
-      </div>
-    </>
+    <div className="w-full justify-items-center">
+      <Landing />
+    </div>
   );
 }
---
+++ next-client/src/components/landing/Landing.tsx
@@ -0,0 +1,226 @@
+import React from "react";
+import { Col, Row } from "../layout";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Card,
+  CardContent,
+} from "../elements";
+import Icons from "@src/assets/icons";
+import {
+  BackgroundAccentClass,
+  BorderClass,
+} from "@src/lib/hooks/useTopicTheme";
+import LandingHero from "@src/assets/hero/LandingHero";
+
+export default function Landing() {
+  return (
+    <Col gap={4} className="max-w-[896px]">
+      <Title />
+      <div className="px-4">
+        <LandingHero className="w-full h-auto" />
+      </div>
+      <About />
+      <AudreyTangQuote />
+      <Organizations />
+      <HowItWorks />
+    </Col>
+  );
+}
+
+const Title = () => (
+  <Col className="p-8" gap={2}>
+    <h2>Talk to the City</h2>
+    <p className="text-muted-foreground">
+      An open-source AI tool that transforms large-scale public input into
+      nuanced insights while preserving individual voices
+    </p>
+  </Col>
+);
+
+const About = () => (
+  <Col className="p-8" gap={2}>
+    <h4>About</h4>
+    <p>
+      Leaders need to understand their communities, but traditional methods
+      either sacrifice depth for scale or scale for depth. Talk to the City
+      (T3C) bridges this gap with an open-source AI platform that turns
+      large-scale conversations into actionable insights while preserving
+      individual perspectives—from local town halls to national policy
+      discussions.
+    </p>
+  </Col>
+);
+
+const AudreyTangQuote = () => (
+  <Col gap={4} className="p-8 md:flex-row gap-x-4">
+    <Avatar className="h-20 w-20">
+      <AvatarImage src={"/images/audrey-tang.png"} />
+      <AvatarFallback>Audrey</AvatarFallback>
+    </Avatar>
+    <Card>
+      <CardContent className="sm:p-6">
+        <Row gap={3}>
+          <div className="self-start flex-shrink-0 py-[5px]">
+            <Icons.Quote className={"h-4 w-4 fill-muted-foreground"} />
+          </div>
+          <Col>
+            <p className="text-muted-foreground">
+              “Ten years ago, the vTaiwan scope was limited, because stakeholder
+              groups needed to adapt to the technology at the time. Today, this
+              is being bridged with the advent of language models that can adapt
+              to their needs. Back in 2014, it was impossible with the capacity
+              of g0v contributors to interview a mini public of people and
+              aggregate their ideas while preserving the full nuance. But now,
+              with Talk to the City’s help, that cost has been reduced to
+              essentially zero. It’s broad-listening and it can change the
+              nature of this recursive public.”
+            </p>
+            <p className="text-muted-foreground">
+              - Audrey Tang, Taiwan’s 1st Digital Minister and co-author of{" "}
+              <a
+                target="_blank"
+                className="underline"
+                href="https://www.plurality.net/"
+              >
+                Plurality.net
+              </a>
+            </p>
+          </Col>
+        </Row>
+      </CardContent>
+    </Card>
+  </Col>
+);
+
+const Organizations = () => (
+  <Col gap={6} className="p-8">
+    <h4>Organizations</h4>
+    <Col gap={4} className="md:flex-row gap-x-6">
+      <OrgCard
+        backgroundColor="bg-theme_purple-accent"
+        borderColor="border-theme_purple"
+        title="Community-driven organizations"
+        points={[
+          "Raise topics for deliberaton",
+          "Empower members to participate in shaping the organization",
+          "Streamline decision making processes",
+        ]}
+        footer="Example orgs: Activist, Grass roots, DAOs"
+      />
+      <OrgCard
+        backgroundColor="bg-theme_greenLeaf-accent"
+        borderColor="border-theme_greenLeaf"
+        title="Government and policy"
+        points={[
+          "Understand what your public believes and needs to inform policy",
+          "Increase speed and clarity of decision making",
+          "Decrease cost and time of analyzing large amounts of qualitative data",
+        ]}
+        footer="Example orgs: Government groups, Policy analysts, Policy evaluators"
+      />
+      <OrgCard
+        backgroundColor="bg-theme_blueSky-accent"
+        borderColor="border-theme_blueSky"
+        title="Enterprise"
+        points={[
+          "Make faster, more agile decisions",
+          "Keep a finger on the pulse of your organization",
+          "Empower staff to participate in shaping the organization",
+        ]}
+        footer="Example orgs: Executives, HR, Analysts"
+      />
+    </Col>
+  </Col>
+);
+
+const OrgCard = ({
+  title,
+  points,
+  footer,
+  backgroundColor,
+  borderColor,
+}: {
+  title: string;
+  points: string[];
+  backgroundColor: BackgroundAccentClass;
+  borderColor: BorderClass;
+  footer: string;
+}) => (
+  <Col
+    gap={3}
+    className={`flex-1 py-4 px-[18px] ${backgroundColor} ${borderColor} border-[1px]`}
+  >
+    <p className="p-medium">{title}</p>
+    <ul className="list-disc pl-4 flex-grow">
+      {points.map((point) => (
+        <li key={point}>{point}</li>
+      ))}
+    </ul>
+    <p className="p2">{footer}</p>
+  </Col>
+);
+
+const HowItWorks = () => (
+  <Col className="p-8" gap={2}>
+    <h4>How it works?</h4>
+    <Row gap={2} className="items-center">
+      <NumSignpost
+        backgroundColor="bg-theme_yellow-accent"
+        borderColor="border-theme_violet"
+        num={1}
+      />
+      <p className="p-medium">Gather</p>
+    </Row>
+    <p className="text-muted-foreground">
+      Gather and analyze qualitative data from a group of any size. Process
+      unstructured text, long form responses, and video interviews, or prompt
+      participants over Whatsapp with a custom chatbot.
+    </p>
+
+    <Row gap={2} className="items-center">
+      <NumSignpost
+        backgroundColor="bg-theme_blueSea-accent"
+        borderColor="border-theme_blueSea"
+        num={2}
+      />
+      <p className="p-medium">Explore</p>
+    </Row>
+
+    <p className="text-muted-foreground">
+      Explore from core themes within the data all the way down to individual
+      claims and quotes made by each participant.
+    </p>
+
+    <Row gap={2} className="items-center">
+      <NumSignpost
+        backgroundColor="bg-theme_purple-accent"
+        borderColor="border-theme_purple"
+        num={3}
+      />
+      <p className="p-medium">Share</p>
+    </Row>
+
+    <p className="text-muted-foreground">
+      Share your findings and the voices within your communities to inform
+      direction and decision making.
+    </p>
+  </Col>
+);
+
+const NumSignpost = ({
+  backgroundColor,
+  borderColor,
+  num,
+}: {
+  backgroundColor: BackgroundAccentClass;
+  borderColor: BorderClass;
+  num: number;
+}) => (
+  <div
+    className={`h-6 w-6 rounded-sm ${borderColor} ${backgroundColor} border-[1px]`}
+  >
+    <p className="p-medium justify-self-center">{num}</p>
+  </div>
+);
