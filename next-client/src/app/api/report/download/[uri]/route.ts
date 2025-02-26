c91adcfb9c7e5d4468f55b8ae91b1ea32526c31c
Brandon Perry <brandonperryofficial@gmail.com>
2025-02-26 10:53:56 -0800

    added await to params in the download api route

--- next-client/src/app/api/report/download/[uri]/route.ts
+++ next-client/src/app/api/report/download/[uri]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(_: Request, { params }: { params: { uri: string } }) {
-  const encodedUri = params.uri;
+  const encodedUri = await params.uri; // we get an error saying that params needs to be awaited? Add await here until it's clearer as to what's going on.
   const uri = decodeURIComponent(encodedUri);
   const jsonData = await fetch(uri, {
     method: "GET",
