fd5f8e98c80ad7c2fedfae7a34c0eb0a6afbd2ee
Brandon Perry <brandonperryofficial@gmail.com>
2025-02-27 15:36:45 -0800

    added some addon types to the schema

--- common/schema.ts
+++ common/schema.ts
@@ -388,6 +388,40 @@ export const questionAnswer = z.object({
 export type QuestionAnswer = z.infer<typeof questionAnswer>;
 
 /********************************
+ * Addons
+ * Extra data that can be attached for special features
+ ********************************/
+
+export const crux = z.object({
+  score: z.number(),
+  cruxA: z.string(),
+  cruxB: z.string(),
+});
+
+export type Crux = z.infer<typeof crux>;
+
+const controversyMatrix = z.array(z.number()).array();
+
+export type ControversyMatrix = z.infer<typeof controversyMatrix>;
+
+export const cruxClaim = z.object({
+  cruxClaim: z.string(),
+  agree: z.array(z.string()),
+  disagree: z.array(z.string()),
+  explanation: z.string(),
+});
+
+export type CruxClaim = z.infer<typeof cruxClaim>;
+
+export const addOns = z.object({
+  topCruxes: crux.array().optional(),
+  controversyMatrix: controversyMatrix.optional(),
+  cruxClaims: cruxClaim.array().optional(),
+});
+
+export type AddOns = z.infer<typeof addOns>;
+
+/********************************
  * Report Data
  * Contains all the information that a report needs to display
  ********************************/
@@ -396,6 +430,7 @@ export const reportDataObj = z.object({
   title: z.string(),
   description: z.string(),
   questionAnswers: z.optional(questionAnswer.array()),
+  addOns: addOns.optional(),
   topics: z.array(topic).transform((topics) =>
     topics.sort((a, b) => {
       const setSpeakersA = new Set(
