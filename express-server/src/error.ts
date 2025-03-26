1775df587cc821d077ac5fa131b0ff0f55bec354
Brandon Perry <brandonperryofficial@gmail.com>
2025-03-26 12:36:12 -0700

    created storage class and a custom errors file. Breaks backend at the moment

---
+++ express-server/src/error.ts
@@ -0,0 +1,33 @@
+/**
+ * Abstract Class to customize errors
+ *
+ * We can use this so that we can more explicitly handle errors using discriminated unions
+ *
+ * example:
+ *
+ * class DoneGoofedError extends CustomError<"You done goofed"> {
+ *  construct(err?:unknown) {
+ *      super("You done goofed", err);
+ *  }
+ * }
+ */
+export abstract class CustomError<T extends string> extends Error {
+  public readonly _tag: T;
+
+  constructor(tag: T, err?: unknown) {
+    const message =
+      err instanceof Error
+        ? `${err.name}: ${err.message}`
+        : `Error: ${String(err)}`;
+    super(message);
+
+    // Set the prototype explicitly
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    // Initialize the _tag property
+    this._tag = tag;
+
+    // Set name for better stack traces
+    this.name = this.constructor.name;
+  }
+}
--- express-server/src/storage.ts
+++ express-server/src/storage.ts
@@ -1,39 +1,100 @@
-import { Storage } from "@google-cloud/storage";
+import {
+  Storage as BucketStorage,
+  Bucket as GBucket,
+} from "@google-cloud/storage";
+import { CustomError } from "./error";
+import * as schema from "tttc-common/schema";
+import { z } from "zod";
 
-let storage: any;
-const encoded_creds = process.env.GOOGLE_CREDENTIALS_ENCODED;
-if (encoded_creds) {
-  const decoded = Buffer.from(encoded_creds, "base64").toString("utf-8");
-  storage = new Storage({ credentials: JSON.parse(decoded) });
+const fileContent = z.union([schema.pipelineOutput, schema.uiReportData]);
+type FileContent = z.infer<typeof fileContent>;
+
+/**
+ * Abstract class for anything that involves storing reports
+ */
+export abstract class Storage {
+  abstract get(fileName: string): Promise<FileContent | StorageGetError>;
+  abstract save(
+    fileName: string,
+    fileContent: string,
+  ): Promise<string | StorageSaveError>;
 }
 
-const bucketName = process.env.GCLOUD_STORAGE_BUCKET;
+/**
+ * Google Cloud Storage Buckets
+ */
+export class Bucket extends Storage {
+  private name: string;
+  private storage: BucketStorage;
+  private bucket: GBucket;
 
-export const getStorageUrl = (fileName: string) =>
-  `https://storage.googleapis.com/${bucketName}/${fileName}`;
+  constructor(encoded_creds: string, name: string) {
+    super();
+    this.name = name;
+    this.storage = new BucketStorage({
+      credentials: this.decodeCredentials(encoded_creds),
+    });
+    this.bucket = this.storage.bucket(this.name);
+  }
 
-export async function storeJSON(
-  fileName: string,
-  fileContent: string,
-  allowCache?: boolean,
-) {
-  if (!bucketName) {
-    throw new Error("Missing bucket name (GCLOUD_STORAGE_BUCKET).");
+  private decodeCredentials = (encoded_creds: string) =>
+    JSON.parse(Buffer.from(encoded_creds, "base64").toString("utf-8"));
+  private storageUrl = (fileName: string) =>
+    `https://storage.googleapis.com/${this.name}/${fileName}`;
+
+  /**
+   * Gets report data from storage bucket. Returns either the content or an error value
+   */
+  async get(fileName: string): Promise<FileContent | StorageGetError> {
+    try {
+      const data = await this.bucket.file(this.storageUrl(fileName)).download();
+      const parsed = fileContent.safeParse(data);
+      if (parsed.success) {
+        return parsed.data;
+      } else {
+        return new InvalidJSONFormat(parsed.error);
+      }
+    } catch (e) {
+      return new BucketGetError(e);
+    }
   }
-  if (!storage) {
-    throw new Error("Missing google creds (GOOGLE_CREDENTIALS_ENCODED)");
+
+  /**
+   * Saves data to storage bucket. Returns either the storage url or an error.
+   */
+  async save(fileName: string, fileContent: string) {
+    const file = this.bucket.file(fileName);
+    try {
+      await file.save(fileContent, {
+        metadata: {
+          contentType: "application/json",
+        },
+      });
+      return this.storageUrl(fileName);
+    } catch (e) {
+      return new BucketSaveError(e);
+    }
   }
-  const bucket = storage.bucket(bucketName);
-  const file = bucket.file(fileName);
-  await file.save(fileContent, {
-    metadata: {
-      contentType: "application/json",
-      ...(allowCache
-        ? {}
-        : {
-            cacheControl: "no-cache, no-store, must-revalidate",
-          }),
-    },
-  });
-  return getStorageUrl(fileName);
+}
+
+type StorageGetError = BucketGetError | InvalidJSONFormat;
+
+type StorageSaveError = BucketSaveError;
+
+class BucketGetError extends CustomError<"BucketGetError"> {
+  constructor(err?: unknown) {
+    super("BucketGetError", err);
+  }
+}
+
+class InvalidJSONFormat extends CustomError<"InvalidJSONFormat"> {
+  constructor(err?: unknown) {
+    super("InvalidJSONFormat", err);
+  }
+}
+
+class BucketSaveError extends CustomError<"BucketSaveError"> {
+  constructor(err?: unknown) {
+    super("BucketSaveError", err);
+  }
 }
