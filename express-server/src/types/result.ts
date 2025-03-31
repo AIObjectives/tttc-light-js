cdc9873209fb0f8e3735e2f03cd8c03e34235030
Brandon Perry <brandonperryofficial@gmail.com>
2025-03-31 14:29:21 -0700

    refactored claimsStep

--- express-server/src/types/result.ts
+++ express-server/src/types/result.ts
@@ -9,3 +9,13 @@ type Failure<T> = {
 };
 
 export type Result<S, F> = Success<S> | Failure<F>;
+
+export function pipe<T, F, T2, F2>(
+  result: Result<T, F>,
+  cb: (arg: T) => Result<T2, F2>,
+): Result<T2, F | F2> {
+  if (result.tag === "failure") return result;
+  else {
+    return cb(result.value);
+  }
+}
