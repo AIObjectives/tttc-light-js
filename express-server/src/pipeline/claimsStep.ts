772abec4015220ba5f4a62fa202fbd79716e2c3e
Portia <portia@portiaportia.com>
2025-04-03 13:36:06 -0700

    refactor: update express server to use header-based API key auth

--- express-server/src/pipeline/claimsStep.ts
+++ express-server/src/pipeline/claimsStep.ts
@@ -3,6 +3,7 @@ import { ClaimsStep } from "./types";
 import { Env } from "../types/context";
 import { Client } from "undici";
 import { AbortController } from "abort-controller"; // If needed in your environment
+
 export async function claimsPipelineStep(env: Env, input: ClaimsStep["data"]) {
   try {
     //console.log("Claims pipeline input:", JSON.stringify(input));
@@ -39,6 +40,7 @@ export async function claimsPipelineStep(env: Env, input: ClaimsStep["data"]) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          [apiPyserver.OPENAI_API_KEY_HEADER]: env.OPENAI_API_KEY,
         },
         body: JSON.stringify(input),
         //signal: controller.signal,
