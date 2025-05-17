a3e0113b2f33f45f38df99320a17157e06bfce9e
Justin Stimatze <justin@objective.is>
2025-05-16 18:22:11 -0700

    T3C-249: Change Firebase config from buildtime to runtime

--- next-client/src/lib/firebase/clientApp.ts
+++ next-client/src/lib/firebase/clientApp.ts
@@ -4,11 +4,9 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { validatedFirebaseConfig } from "./config";
+import { firebaseConfig } from "./config";
 export const firebaseApp =
-  getApps().length === 0
-    ? initializeApp(validatedFirebaseConfig)
-    : getApps()[0];
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
 export const storage = getStorage(firebaseApp);
