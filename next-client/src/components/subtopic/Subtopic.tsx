40a2eaf70ec3f3187088e589e52b167910785cad
Brandon Perry <brandonperryofficial@gmail.com>
2025-05-27 10:32:57 -0700

    T3C-269 Refactor sub-topic component

--- next-client/src/components/subtopic/Subtopic.tsx
+++ next-client/src/components/subtopic/Subtopic.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useContext } from "react";
+import React, { forwardRef } from "react";
 import * as schema from "tttc-common/schema";
 import { CopyLinkButton } from "../copyButton/CopyButton";
 import { ExpandableText, Separator, TextIcon } from "../elements";
@@ -8,39 +8,29 @@ import { Col, Row } from "../layout";
 import Icons from "@/assets/icons";
 import ClaimLoader from "./components/ClaimLoader";
 import { getNPeople } from "tttc-common/morphisms";
-import { ReportContext } from "../report/Report";
-import { SubtopicNode } from "../report/hooks/useReportState";
-import { mergeRefs } from "react-merge-refs";
-import { NodeWrapper } from "../node/NodeWrapper";
-
-/**
- * Subtopic Node. Only show contents if isOpen=true.
- */
-function Subtopic({ node, isOpen }: { node: SubtopicNode; isOpen: boolean }) {
-  const { useScrollTo, useFocusedNode } = useContext(ReportContext);
-
-  const scrollRef = useScrollTo(node.data.id);
-  const focusedRef = useFocusedNode(node.data.id, !isOpen);
-
-  return (
-    <div ref={mergeRefs([scrollRef, focusedRef])}>
-      {isOpen && <SubtopicComponent subtopicNode={node} />}
-    </div>
-  );
-}
+import { ClaimNode, SubtopicNode } from "../report/hooks/useReportState";
+import { ClaimItem } from "../claim/ClaimItem";
 
 /**
  * UI for subtopic
  */
-const SubtopicComponent = forwardRef<
+export const Subtopic = forwardRef<
   HTMLDivElement,
-  { subtopicNode: SubtopicNode }
->(function TopicComponent({ subtopicNode }, ref) {
+  { subtopicNode: SubtopicNode; onExpandSubtopic: () => void }
+>(function TopicComponent({ subtopicNode, onExpandSubtopic }, ref) {
   return (
     <div data-testid={"subtopic-item"}>
       <Col gap={4} className="py-6 sm:py-8" ref={ref}>
-        <SubtopicSummary subtopic={subtopicNode.data} />
-        <SubtopicClaims subtopicNode={subtopicNode} />
+        <SubtopicSummary
+          title={subtopicNode.data.title}
+          description={subtopicNode.data.description}
+          claims={subtopicNode.data.claims}
+        />
+        <SubtopicClaims
+          claimNodes={subtopicNode.children}
+          pagination={subtopicNode.pagination}
+          onExpandSubtopic={onExpandSubtopic}
+        />
       </Col>
       <Separator />
     </div>
@@ -51,12 +41,10 @@ export function SubtopicHeader({
   title,
   numClaims,
   numPeople,
-  button,
 }: {
   title: string;
   numClaims: number;
   numPeople: number;
-  button?: React.ReactNode;
 }) {
   return (
     <Row gap={4} className="justify-between items-center">
@@ -68,7 +56,7 @@ export function SubtopicHeader({
       <TextIcon icon={<Icons.Claim />}>
         {numClaims} claims by {numPeople} people
       </TextIcon>
-      {button}
+      <CopyLinkButton anchor={title} />
     </Row>
   );
 }
@@ -77,45 +65,54 @@ export function SubtopicDescription({ description }: { description: string }) {
   return <ExpandableText>{description}</ExpandableText>;
 }
 
-export function SubtopicSummary({ subtopic }: { subtopic: schema.Subtopic }) {
-  const { title, claims, description } = subtopic;
+export function SubtopicSummary({
+  title,
+  claims,
+  description,
+}: {
+  title: string;
+  claims: schema.Claim[];
+  description: string;
+}) {
   return (
     <Col gap={4} className="px-4 sm:px-8">
       <SubtopicHeader
         title={title}
         numClaims={claims.length}
         numPeople={getNPeople(claims)}
-        button={<CopyLinkButton anchor={title} />}
       />
-      <PointGraphic claims={subtopic.claims} />
+      <PointGraphic claims={claims} />
       <SubtopicDescription description={description!} />
     </Col>
   );
 }
 
 export function SubtopicClaims({
-  subtopicNode,
+  claimNodes,
+  onExpandSubtopic,
+  pagination,
 }: {
-  subtopicNode: SubtopicNode;
+  claimNodes: ClaimNode[];
+  pagination: number;
+  onExpandSubtopic: () => void;
 }) {
   return (
     <Col>
       <p className="leading-6 pl-8 text-base font-medium">Claims</p>
       <Col gap={4}>
         <Col>
-          {subtopicNode.children.map((claimNode, i) => {
+          {claimNodes.map((claimNode, i) => {
             return (
-              <NodeWrapper
-                key={claimNode.id}
-                node={claimNode}
-                className={`${i <= subtopicNode.pagination ? "" : "hidden"}`}
-              >
+              <ClaimItem show={i <= pagination} id={claimNode.id}>
                 <Claim claim={claimNode.data} />
-              </NodeWrapper>
+              </ClaimItem>
             );
           })}
         </Col>
-        <ClaimLoader subtopicNode={subtopicNode} />
+        <ClaimLoader
+          remaining={claimNodes.length - pagination - 1}
+          onExpandSubtopic={onExpandSubtopic}
+        />
       </Col>
     </Col>
   );
