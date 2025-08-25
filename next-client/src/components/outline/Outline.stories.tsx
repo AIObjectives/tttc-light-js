4fc2aeba858218e8e82a252c7e04a0c31502e3d0
Brandon Perry <49536689+Brandon-Perry@users.noreply.github.com>
2025-08-25 08:58:30 -0700

    added chromatic github action (#251)

--- next-client/src/components/outline/Outline.stories.tsx
+++ next-client/src/components/outline/Outline.stories.tsx
@@ -1,28 +1,46 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { reportData } from "../../../stories/data/dummyData";
-import Outline from "./Outline";
-import { __internals } from "./hooks/useOutlineState";
-import { stateBuilder } from "../report/hooks/useReportState/utils";
-import { createInitialState } from "./hooks/useOutlineState/utils";
+// import { reportData } from "../../../stories/data/dummyData";
+// import Outline from "./Outline";
+// import { __internals } from "./hooks/useOutlineState";
+// import { stateBuilder } from "../report/hooks/useReportState/utils";
+// import { createInitialState } from "./hooks/useOutlineState/utils";
+
+// const meta = {
+//   title: "Outline",
+//   component: Outline,
+//   parameters: {
+//     layout: "center",
+//   },
+//   tags: ["autodocs"],
+// } satisfies Meta<typeof Outline>;
+
+// const reportState = stateBuilder(reportData.topics);
+// const outlineState = createInitialState(reportState.children);
+// export default meta;
+// type Story = StoryObj<typeof meta>;
+
+// export const Main: Story = {
+//   args: {
+//     outlineState,
+//     reportDispatch: () => {},
+//     outlineDispatch: () => {},
+//   },
+// };
+
+/**
+ * @fileoverview
+ *
+ * ! Removed outline story temporarily because it was causing Storybook to crash
+ */
+
+const dummy = () => <p>Removed temporarily</p>;
 
 const meta = {
   title: "Outline",
-  component: Outline,
-  parameters: {
-    layout: "center",
-  },
-  tags: ["autodocs"],
-} satisfies Meta<typeof Outline>;
-
-const reportState = stateBuilder(reportData.topics);
-const outlineState = createInitialState(reportState.children);
+  component: dummy,
+} satisfies Meta<typeof dummy>;
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const Main: Story = {
-  args: {
-    outlineState,
-    reportDispatch: () => {},
-    outlineDispatch: () => {},
-  },
-};
+export const Default: Story = {};
