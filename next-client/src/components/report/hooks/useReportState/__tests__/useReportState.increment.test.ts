12cd89e8114edf6a258a5f8de76dee983a117b88
Justin Stimatze <justin@objective.is>
2025-08-25 08:02:51 -0700

    Fix report state tests again

--- next-client/src/components/report/hooks/useReportState/__tests__/useReportState.increment.test.ts
+++ next-client/src/components/report/hooks/useReportState/__tests__/useReportState.increment.test.ts
@@ -9,23 +9,27 @@ import {
   defaultTopicPagination,
 } from "../consts";
 import { ReportState, SubtopicNode, TopicNode } from "../types";
+import { mapIdsToPath } from "../path";
+import { createPathMapReducer } from "../reducer";
 
-const { state, reducer } = setupTestState();
+const { state } = setupTestState();
 
 const getTestTopic = (state: ReportState): TopicNode => state.children[0];
 
 const getTestSubtopic = (state: ReportState): SubtopicNode =>
   getTestTopic(state).children[0];
 
-const incrementTopic = (reportState: ReportState, id: string) =>
-  reducer(reportState, { type: "expandTopic", payload: { id } });
+const incrementTopic = (reportState: ReportState, id: string) => {
+  const reducer = createPathMapReducer(mapIdsToPath(reportState));
+  return reducer(reportState, { type: "expandTopic", payload: { id } });
+};
 
-const incrementSubtopic = (reportState: ReportState, id: string) =>
-  reducer(reportState, { type: "expandSubtopic", payload: { id } });
+const incrementSubtopic = (reportState: ReportState, id: string) => {
+  const reducer = createPathMapReducer(mapIdsToPath(reportState));
+  return reducer(reportState, { type: "expandSubtopic", payload: { id } });
+};
 
 describe("Topic Node", () => {
-  const newState1 = incrementTopic(state, getTestTopic(state).id);
-  const newState2 = incrementTopic(newState1, getTestTopic(newState1).id);
   describe("Precheck", () => {
     test("Make sure our test topic's children length is >= 3", () => {
       expect(getTestTopic(state).children.length).greaterThanOrEqual(3);
@@ -37,15 +41,21 @@ describe("Topic Node", () => {
   });
 
   test("Incrementing once results in the topic's pagination going up by the set amount", () => {
+    const newState1 = incrementTopic(state, getTestTopic(state).id);
     expect(getTestTopic(newState1).pagination).toBe(
       defaultTopicPagination + defaultAddTopicPagination,
     );
   });
 
   test("Incrementing twice works", () => {
-    expect(getTestTopic(newState2).pagination).toBe(
+    const newState1 = incrementTopic(state, getTestTopic(state).id);
+    const newState2 = incrementTopic(newState1, getTestTopic(newState1).id);
+    // Should be min of (1 + 1*2 = 3) or (children.length - 1)
+    const expectedPagination = Math.min(
       defaultTopicPagination + defaultAddTopicPagination * 2,
+      getTestTopic(state).children.length - 1,
     );
+    expect(getTestTopic(newState2).pagination).toBe(expectedPagination);
   });
 
   test("Caps at children's length", () => {
@@ -54,7 +64,7 @@ describe("Topic Node", () => {
     const newState = pipe(
       Array.replicate(incrementTopic, maxLen),
       Array.reduce(state, (accumState, f) =>
-        f(accumState, getTestTopic(state).id),
+        f(accumState, getTestTopic(accumState).id),
       ),
     );
     expect(getTestTopic(newState).pagination).toBe(maxLen);
@@ -62,10 +72,6 @@ describe("Topic Node", () => {
 });
 
 describe("Subtopic Node", () => {
-  const maxLen = getTestSubtopic(state).children.length - 1;
-  const newState1 = incrementSubtopic(state, getTestSubtopic(state).id);
-  const newState2 = incrementSubtopic(newState1, getTestSubtopic(state).id);
-
   describe("Precheck", () => {
     test("Make sure our test subtopic's children legnth is >= 10", () => {
       expect(getTestSubtopic(state).children.length).toBeGreaterThanOrEqual(10);
@@ -77,12 +83,18 @@ describe("Subtopic Node", () => {
   });
 
   test("Incrementing once results in default + add amount", () => {
+    const newState1 = incrementSubtopic(state, getTestSubtopic(state).id);
     expect(getTestSubtopic(newState1).pagination).toBe(
       defaultSubtopicPagination + defaultAddSubtopicPagination,
     );
   });
 
   test("Incrementing twice results in appropriate amount", () => {
+    const newState1 = incrementSubtopic(state, getTestSubtopic(state).id);
+    const newState2 = incrementSubtopic(
+      newState1,
+      getTestSubtopic(newState1).id,
+    );
     expect(getTestSubtopic(newState2).pagination).toBe(
       defaultSubtopicPagination + defaultAddSubtopicPagination * 2,
     );
@@ -90,10 +102,11 @@ describe("Subtopic Node", () => {
 
   test("Caps at childrens length", () => {
     // apply increment a ton of times and make sure it caps at right point
+    const maxLen = getTestSubtopic(state).children.length - 1;
     const newState = pipe(
       Array.replicate(incrementSubtopic, maxLen),
       Array.reduce(state, (accumState, f) =>
-        f(accumState, getTestSubtopic(state).id),
+        f(accumState, getTestSubtopic(accumState).id),
       ),
     );
     expect(getTestSubtopic(newState).pagination).toBe(maxLen);
