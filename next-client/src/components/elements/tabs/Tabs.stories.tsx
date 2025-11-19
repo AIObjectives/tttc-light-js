d271f28a6c44595d3a613beb0a038d1d02858385
Justin Stimatze <justin@objective.is>
2025-11-18 19:45:03 -0800

    T3C-776: Resolve glob security vulnerability by removing Babel dependencies
    - Replace @babel/cli with tsc in express-server and pipeline-worker
    - Upgrade Storybook from 8.x to 10.x in next-client
    - Remove deprecated Storybook addon packages (essentials, interactions, links, blocks, test)
    - Update express-server tsconfig target to ES2022
    - Delete babel.config.json files

--- next-client/src/components/elements/tabs/Tabs.stories.tsx
+++ next-client/src/components/elements/tabs/Tabs.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
+import { fn } from "storybook/test";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./Tabs";
 import "../../../app/global.css";
 
