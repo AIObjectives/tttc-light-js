f88b5900ec54d7ab37c5a75eb82e22b0630b14ae
Justin Stimatze <justin@objective.is>
2025-12-04 11:41:17 -0800

    T3C-798: Standardize overlay opacities

--- next-client/src/components/elements/hoverCard/HoverCard.tsx
+++ next-client/src/components/elements/hoverCard/HoverCard.tsx
@@ -18,7 +18,7 @@ const HoverCardOverlay = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "fixed inset-0 z-40 bg-black/20 pointer-events-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-40 bg-black/25 pointer-events-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className,
     )}
     {...props}
