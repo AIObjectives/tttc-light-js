aaf410a3cdc7a2af61d2c2370deeed5df2a061e7
Justin Stimatze <justin@objective.is>
2025-12-12 22:00:18 -0800

    T3C-283: Fix hover clipping

--- next-client/src/components/claim/HoverQuoteCard.tsx
+++ next-client/src/components/claim/HoverQuoteCard.tsx
@@ -101,7 +101,7 @@ function HoverQuoteCard({
           ref={contentRef}
           sideOffset={-1}
           side="top"
-          avoidCollisions={false}
+          collisionPadding={16}
         >
           <QuoteCard claim={claim} />
         </HoverCardContent>
