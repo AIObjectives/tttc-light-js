b2ad444dfb8aa13a68dc6d6854b7b11e4a0ff589
Bill Gallagher <bill@objective.is>
2026-04-28 14:56:11 -0700

    new landing page

--- next-client/src/app/page.tsx
+++ next-client/src/app/page.tsx
@@ -1,17 +1,24 @@
 import Feedback from "@/components/feedback/Feedback";
 import Landing from "@/components/landing/Landing";
+import LandingRedesign from "@/components/landing/LandingRedesign";
 import { serverSideAnalyticsClient } from "@/lib/analytics/serverSideAnalytics";
+import {
+  initializeFeatureFlags,
+  isFeatureEnabled,
+} from "@/lib/feature-flags/featureFlags.server";
 
 export function generateStaticParams() {
   return [{ slug: [""] }];
 }
 
 export default async function HomePage() {
+  initializeFeatureFlags();
   const analytics = await serverSideAnalyticsClient();
   await analytics.page("Home");
+  const redesignEnabled = await isFeatureEnabled("website-redesign");
   return (
     <div>
-      <Landing />
+      {redesignEnabled ? <LandingRedesign /> : <Landing />}
       <Feedback />
     </div>
   );
