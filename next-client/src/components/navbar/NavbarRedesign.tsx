2bea5be7acb5236ff2e939c3782efa4d7c835c33
Allegra-Cohen <allegra.ab.cohen@gmail.com>
2026-05-27 15:45:05 -0400

    T3C-1155 T3C-1156 Add /help (transcription guide) to front-facing routes

--- next-client/src/components/navbar/NavbarRedesign.tsx
+++ next-client/src/components/navbar/NavbarRedesign.tsx
@@ -7,7 +7,14 @@ import UserFacingNavbar from "./UserFacingNavbar";
 // Routes that show the marketing / front-facing menu. Everything else gets the
 // user-facing (app) menu. New marketing pages added later (e.g. /privacy) should
 // be added here.
-const FRONT_FACING_ROUTES = ["/", "/about", "/safety", "/product", "/pricing"];
+const FRONT_FACING_ROUTES = [
+  "/",
+  "/about",
+  "/safety",
+  "/product",
+  "/pricing",
+  "/help",
+];
 
 function isFrontFacing(pathname: string | null): boolean {
   if (!pathname) return true;
